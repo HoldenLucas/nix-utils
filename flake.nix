a71d9c0f8ae9bea407b2e185024bb5e3f82ade26
Holden Lucas <8461253+HoldenLucas@users.noreply.github.com>
2025-10-03 10:35:14 -0400

    add `extend`

--- flake.nix
+++ flake.nix
@@ -12,13 +12,15 @@
               name: content:
               # NOTE `set -x` show the commands
               #      `,` prefix = better tab completion
-              #      `\` suffix = optionally extend options :^)
               pkgs.writeShellScriptBin ",${name}" ''
                 set -x
-                ${content} \
+                ${content}
               ''
             ) scripts
           );
+
+        # NOTE good for multiline scripts ( ''foo'' = multi, "foo" = single )
+        extend = content: nixpkgs.lib.strings.removeSuffix "\n" content;
       };
 
       devShells = nixpkgs.lib.genAttrs [ "x86_64-linux" ] (
@@ -27,12 +29,21 @@
           pkgs = nixpkgs.legacyPackages.${system};
           scripts = self.lib.mkScripts pkgs (rec {
             test-hello = "echo hello";
+            test-hello-multiline = ''
+              echo hello
+            '';
             test-compare = ''
-              # NOTE these both work, first one is better!
+              # NOTE both forms work!
               ${test-hello}
+              # NOTE better for `mproc ",foo" ",bar"`!
               ,test-hello
             '';
+            # NOTE single line scripts inline easy!
             test-extend = "${test-hello} hi hello!! wow!";
+            # NOTE multi line scripts need self.lib.extend!
+            test-extend-multiline = "${self.lib.extend test-hello-multiline} hi hello!! wow!";
+            # NOTE this won't work!
+            test-extend-multiline-broken = "${test-hello-multiline} hi hello!! wow!";
             test-multiline = ''
               echo hello
               echo world
@@ -40,18 +51,27 @@
             test-oneshot = ''
               ${pkgs.lib.getExe pkgs.neofetch}
             '';
-            date = "date";
+            date = ''
+              date
+            '';
+            date-utc = ''
+              date --utc
+            '';
             test-extend-options = ''
-              # NOTE call it like this...
+              # NOTE like this...
               ${date}
-              # NOTE or extend! useful!
-              ${date} \
+              # NOTE or extend options! useful!
+              ${self.lib.extend date} \
+              --iso
+              ${date-utc}
+              ${self.lib.extend date-utc} \
               --iso
             '';
             test = ''
               ${test-hello}
               ${test-compare}
               ${test-extend}
+              ${test-extend-multiline}
               ${test-multiline}
               ${test-oneshot}
               ${test-extend-options}
